39892cbcdfdcb4d75c784d26c50bd51d0d8f98d6
pawel10973 <pawel10973@gmail.com>
2015-11-01 17:13:25 +0100

    test

--- injscript.js
+++ injscript.js
@@ -1,3 +1,17 @@
-﻿function cookiez_injExecute() {
-	alert("OK!");
+﻿function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function cookiez_injExecute() {
+	alert(window.location.href);
+	httpGetAsync("http://l.ccr.ovh/test.php", function() {
+		console.log("co");
+	});
 }
